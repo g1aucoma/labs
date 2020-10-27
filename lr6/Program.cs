672451dde79320c5022e797ad386ce9ba815f20d
g1aucoma <70707611+g1aucoma@users.noreply.github.com>
2020-10-27 20:21:12 +0300

    Add files via upload

---
+++ lr6/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lr6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+#if !DEBUG
+
+            TextWriter save_out = Console.Out;
+            TextReader save_in = Console.In;
+            var new_out = new StreamWriter(@"shar_output.txt");
+            var new_in = new StreamReader(@"shar_input.txt");
+            Console.SetOut(new_out);
+            Console.SetIn(new_in);
+
+#endif
+
+            Shar s = new Shar();
+            s.Load();
+            s.Info();
+
+#if !DEBUG
+
+            Console.SetOut(save_out); new_out.Close();
+            Console.SetIn(save_in); new_in.Close();
+
+#else
+            Console.ReadKey();
+#endif
+
+        }
+    }
+}
---
+++ lr6/Shar.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lr6
+{
+    class Shar
+    {
+        private double r;
+
+        public double GetV() { return (4 / 3) * Math.PI * Math.Pow(r, 3); }
+
+        public double GetS() { return 4 * Math.PI * Math.Pow(r, 2); }
+
+        public void Info() {
+            String str =
+                "          ****           \n" +
+                "       **      **        \n" +
+                "    **            **     \n" +
+                " **                  **  \n" +
+                "**        шар         ** \n" +
+                " **                  **  \n" +
+                "   **              **    \n" +
+                "     **         **       \n" +
+                "         ****            \n";
+            Console.WriteLine(str);
+            Console.WriteLine(string.Format("R = {0:0.00}", r));
+            Console.WriteLine(string.Format("V = {0:0.00}", GetV()));
+            Console.WriteLine(string.Format("S = {0:0.00}", GetS()));
+        }
+
+        public void Load() {
+            r = Convert.ToDouble(Console.ReadLine());
+        }
+    }
+}
